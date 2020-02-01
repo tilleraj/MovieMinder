0c319390e6c99de070a90ddab262180d7d28b2bf
Andrew Tiller <tiller.andrew@gmail.com>
2020-02-01 12:50:47 -0600

    implemented RestSharp in a basic way to pull showtimes (MovieSchedules) with a hard-coded zipcode and date. have resuested elevated api access to be able to access by individual movie

---
+++ movieminder.api/movieminder.api/Models/MovieSchedule.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace movieminder.api.Models
+{
+    public class MovieSchedule
+    {
+        public string tmsId { get; set; }
+        public string rootId { get; set; }
+        public string subType { get; set; }
+        public string title { get; set; }
+        public string titleLang { get; set; }
+        public string entityType { get; set; }
+        public PreferredImage preferredImage { get; set; }
+        public List<Showtime> showtimes { get; set; }
+        public int? releaseYear { get; set; }
+        public string releaseDate { get; set; }
+        public string descriptionLang { get; set; }
+        public List<string> genres { get; set; }
+        public string longDescription { get; set; }
+        public string shortDescription { get; set; }
+        public List<string> topCast { get; set; }
+        public List<string> directors { get; set; }
+        public string officialUrl { get; set; }
+        public List<Rating> ratings { get; set; }
+        public List<string> advisories { get; set; }
+        public string runTime { get; set; }
+        public QualityRating qualityRating { get; set; }
+        public string audience { get; set; }
+        public string animation { get; set; }
+
+        public class Caption
+        {
+            public string content { get; set; }
+            public string lang { get; set; }
+        }
+
+        public class PreferredImage
+        {
+            public string uri { get; set; }
+            public string width { get; set; }
+            public string height { get; set; }
+            public Caption caption { get; set; }
+            public string category { get; set; }
+            public string text { get; set; }
+            public string primary { get; set; }
+        }
+
+        public class Theatre
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Showtime
+        {
+            public Theatre theatre { get; set; }
+            public string dateTime { get; set; }
+            public string quals { get; set; }
+            public bool barg { get; set; }
+            public string ticketURI { get; set; }
+        }
+
+        public class Rating
+        {
+            public string body { get; set; }
+            public string code { get; set; }
+        }
+
+        public class QualityRating
+        {
+            public string ratingsBody { get; set; }
+            public string value { get; set; }
+        }
+
+    }
+}
---
+++ movieminder.api/movieminder.api/Repositories/MovieScheduleRepository.cs
@@ -0,0 +1,29 @@
+﻿using movieminder.api.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieminder.api.Repositories
+{
+    public class MovieScheduleRepository
+    {
+        private RestClient _client;
+
+        public MovieScheduleRepository()
+        {
+            _client = new RestClient("http://data.tmsapi.com/v1.1/movies/");
+
+        }
+
+        public List<MovieSchedule> GetAllMovieSchedules()
+        {
+            var request = new RestRequest("showings?startDate=2020-02-01&zip=37205&api_key=3cbn8c8akp4j34afwe8ztvnx");
+
+            var movieSchedules = _client.Get<List<MovieSchedule>>(request);
+
+            return movieSchedules.Data;
+        }
+    }
+}
