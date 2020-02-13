f5ba41d55584829fa267eccb1a02901d5bcf81ef
Andrew Tiller <tiller.andrew@gmail.com>
2020-02-13 06:59:19 -0600

    improved NowShowing styling and made the dateTime just time

--- movieminder.ui/src/components/Showtime/Showtime.js
+++ movieminder.ui/src/components/Showtime/Showtime.js
@@ -6,10 +6,11 @@ class Showtime extends React.Component {
 
   render() {
     const { showtime } = this.props;
+    const time = showtime.dateTime.slice(11);
     return (
       <tr>
         <td>{showtime.theatre.name}</td>
-        <td>{showtime.dateTime}</td>
+        <td>{time}</td>
         <td><a href={showtime.ticketURI}>Tickets</a></td>
       </tr>
     )
