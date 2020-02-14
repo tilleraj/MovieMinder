77a321e0dbe1edfa7de192c593398a6234a16828
Andrew Tiller <tiller.andrew@gmail.com>
2020-02-14 00:41:49 -0600

    moved MyLists to Home

--- movieminder.ui/src/components/Home/Home.js
+++ movieminder.ui/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Button,
 } from 'reactstrap';
-
+import MyLists from '../MyLists/MyLists';
 import Register from '../Register/Register';
 import './Home.scss';
 
@@ -17,12 +17,13 @@ class Home extends React.Component {
           < Register {...this.props} />)
       } else {
         return (
-          <div className="card">
-            <div className="card-body">
-              <button type="button" className="btn btn-primary">This is a Homepage</button>
-              <Button>Welcome {this.props.profile.userName}</Button>
-            </div>
-          </div>
+          // <div className="card">
+          //   <div className="card-body">
+          //     <button type="button" className="btn btn-primary">This is a Homepage</button>
+          //     <Button>Welcome {this.props.profile.userName}</Button>
+          //   </div>
+          // </div>
+          <MyLists authed={this.props.authed} profile={this.props.profile}/>
         );
       }
     }
